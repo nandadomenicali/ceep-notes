8c144fab5c393c108b0095dbd4b38ee7d3557f92
Fernanda Domenicali <nanda21domenicali@gmail.com>
2021-06-21 14:45:21 -0300

    files saved to changed

--- componentes/concluiTarefa.js
+++ componentes/concluiTarefa.js
@@ -14,3 +14,5 @@ const concluirTarefa = (evento) => {
      tarefaCompleta.classList.toggle('done') 
       
 }
+
+export default BotaoConclui
--- main.js
+++ main.js
@@ -23,4 +23,3 @@ import BotaoDeleta from "./componentes/deletaTarefa"
 const novaTarefa = document.querySelector('[data-form-button]')
 
 novaTarefa.addEventListener('click', criarTarefa)
- 
